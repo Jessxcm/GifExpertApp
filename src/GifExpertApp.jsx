3a2eb715631e0296a266e9fda6fc0111d6a070b7
Jessica <calajess@gmail.com>
2024-08-22 14:27:15 -0500

    Se agregan categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
 
 export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  const onAddCategory = () => {
+    setCategories([...categories, 'Valorant' ]);
+    //setcategories(['Valorant', ...categories ]);
+    //setcategories( cat => [...cat, 'Valorant']);
+  }
+
   return (
     <>
-    <h1>GifExpertApp</h1>
+      {/*Titulo*/}
+      <h1>GifExpertApp</h1>
+      {/*Input*/}
+      <AddCategory 
+      setCategories={ setCategories }
+      >
+
+      </AddCategory>
+      {/*Listado de gif*/}
+      <ol>
+        {categories.map((category) => {
+          return <li key={category}> {category}</li>;
+        })}
+      </ol>
+      {/*gif item*/}
     </>
-  )
-}
+  );
+};
