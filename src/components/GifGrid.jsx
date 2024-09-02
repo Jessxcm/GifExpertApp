b6882499192e8c6dc2c6363e9be588af3a3e1b86
Jessica <calajess@gmail.com>
2024-09-02 17:13:49 -0600

    Se agrega custom hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,11 +1,29 @@
-import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "./hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
+
+  const {images, isLoading} = useFetchGifs(category);
+
+  console.log({images, isLoading});
+
 
   return (
     <>
       <h3>{category}</h3>
+      {/* <LoadingMessage isLoading={isLoading}></LoadingMessage> */}
+
+      {
+        isLoading && (<h2> Cargando...</h2>)
+      }
+     
+      <div className="card-grid"> 
+        {images.map((image) => (
+          <GifItem key={image.id}
+          {...image} //exparcir todas las propiedades de image, para no mandar todas por separado y el componente toma en las props las que necesita
+          ></GifItem>
+        ))}
+      </div>
     </>
   );
 };
